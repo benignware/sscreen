e4d41272be615f33d300c28a1dd5cea6d53c9efe
Rafael Nowrotek <mailbenignware.com>
2019-01-14 01:32:38 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const sass = require('node-sass');
+const path = require('path');
+const assert = require('assert');
+const postcss = require('postcss');
+
+describe('sscreen', () => {
+  it('renders correctly', () => {
+    const { css } = sass.renderSync({
+      file: path.resolve(__dirname, './fixtures/src/scss/index.scss'),
+      includePaths: [
+        path.resolve(__dirname, '..'),
+        path.resolve(__dirname, './fixtures/src')
+      ]
+    });
+    const expectedFile = path.join(__dirname, 'expected/index.css');
+    const { plugins } = require(path.resolve(__dirname, 'fixtures/postcss.config.js'));
+
+    postcss(plugins)
+      .process(css, {
+        from: undefined
+      })
+      .then(({ css: actual }) => {
+        let expected = '';
+
+        if (fs.existsSync(expectedFile)) {
+          expected = fs.readFileSync(expectedFile, 'utf-8');
+        } else {
+          expected = actual;
+          fs.mkdirSync(path.join(__dirname, 'expected'));
+          fs.writeFileSync(expectedFile, actual, 'utf-8');
+        }
+
+        assert.equal(actual, expected);
+      });
+  });
+});
